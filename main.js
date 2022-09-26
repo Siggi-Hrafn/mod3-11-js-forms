d698e939d14c7ed43c06c61b348507a29ee79bce
siggihrafnthorkels <89144713+siggihrafnthorkels@users.noreply.github.com>
2022-09-26 15:50:07 +0000

    js added - in progress

---
+++ main.js
@@ -0,0 +1,8 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let name = id("fname"),
+    email = id("email"),
+    subject = id("subject"),
+    message = id("message")
