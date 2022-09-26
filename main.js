fc77fea367c40d895a24c772c75ad39aa5879116
siggihrafnthorkels <89144713+siggihrafnthorkels@users.noreply.github.com>
2022-09-26 15:55:04 +0000

    js funtions and event listeners

--- main.js
+++ main.js
@@ -5,4 +5,16 @@ let classes = (classes) => document.getElementsByClassName(classes);
 let name = id("fname"),
     email = id("email"),
     subject = id("subject"),
-    message = id("message")
+    message = id("message"),
+
+    errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+  
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+
+  let engine = (id, serial, message) => {}
